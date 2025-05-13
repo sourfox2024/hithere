a84e4f4178f08546a2dafe29fafa67d900251c03
Veronica <vronicasan@gmail.com>
2025-05-13 15:52:12 +1000

    fix spaces

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn main() {
     let mut name = String::new();
     match io::stdin().read_line(&mut name) {
         Ok(_) => {
-                    //trims the string
-                    let trim1 = name.trim();
+            //trims the string
+            let trim1 = name.trim();
             println!("Hi {}, nice to meet you.", trim1);
         },
         Err(error) => println!("error: {error}"),
